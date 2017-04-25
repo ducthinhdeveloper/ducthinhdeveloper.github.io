08913d314de56f22698961cb6ed50c9b35f74a03
Timothy <timothy@watcheszon.com>
2017-04-25 20:09:28 +0700

    Update Bookstore

--- controllers/genres.js
+++ controllers/genres.js
@@ -1,9 +1,9 @@
-myApp.controller('BooksController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams){
-	console.log('BooksController loaded...');
-	var root = 'http://localhost:3000';
-	$scope.getGenres = function(){
-		$http.get(root+'/api/genres').success(function(response){
-			$scope.genres = response;
-		});
-	}
+myApp.controller('GenreController', ['$scope', '$http', '$location', '$routeParams', function($scope, $http, $location, $routeParams) {
+    console.log('BooksController loaded...');
+    var root = 'https://green-web-bookstore.herokuapp.com';
+    $scope.getGenres = function() {
+        $http.get(root + '/api/genres').success(function(response) {
+            $scope.genres = response;
+        });
+    }
 }]);
